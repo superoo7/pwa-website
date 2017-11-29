13fc8f8ba7bce166f04ac5fc257d8b8e02ebd1b9
Lai Weng Han <jlwhoo7@gmail.com>
2017-11-29 16:24:24 +0800

    Updates

--- js/nav.js
+++ js/nav.js
@@ -2,22 +2,22 @@ document.getElementById("navbar").innerHTML += `<nav class="light-green lighten-
 <div class="nav-wrapper container">
     <ul id="nav" class="side-nav" style="transform: translateX(-100%);">
         <li>
-            <a href="/">Home</a>
+            <a href="pwa-website">Home</a>
         </li>
         <li>
-            <a href="/add_to_homescreen.html">Add To Home Screen</a>
+            <a href="pwa-website/add_to_homescreen.html">Add To Home Screen</a>
         </li>
         <li>
-            <a href="/push_notification_permission.html">Notification Permisison</a>
+            <a href="push_notification_permission.html">Notification Permisison</a>
         </li>
         <li>
-            <a href="/push_notification.html">Push Notification</a>
+            <a href="push_notification.html">Push Notification</a>
         </li>
         <li>
-            <a href="/weird_form.html">Weird Form</a>
+            <a href="weird_form.html">Weird Form</a>
         </li>
         <li>
-            <a href="/camera.html">Camera</a>
+            <a href="camera.html">Camera</a>
         </li>
     </ul>
     <a href="#" data-activates="nav" class="button-collapse show-on-large">
